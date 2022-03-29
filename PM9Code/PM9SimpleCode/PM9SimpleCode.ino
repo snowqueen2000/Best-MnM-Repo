06fc0a3dc050db83160f15bcf8efc3aa30a7c90d
Hexalv <trenhir@gmail.com>
2022-03-29 11:11:46 -0600

    howdy fellers

--- PM9Code/PM9SimpleCode/PM9SimpleCode.ino
+++ PM9Code/PM9SimpleCode/PM9SimpleCode.ino
@@ -6,6 +6,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+//CHANGE FOR EACH PERSON
+int deviceAddress = 1;
 // Color to sort
 int colorDetect = 1; // R=1, G=2. Bl=3, Br=4, Ye=5, Or=6
 
@@ -31,6 +33,11 @@ int candySorted = 0;
 const int mPWM = 5;
 const int mp1 = 6;
 const int mp2 = 7;
+
+//Hopper motor pins
+const int hopPWM = 3;
+const int hop1 = 1;
+const int hop2 = 2;
 
 // encoder
 Encoder myEnc(18,19); // initialize encoder A out --> 18, B out --> 19
@@ -92,25 +99,25 @@ int vals[3];                 // array to store three color reading
 //double emptyv[6];
 //double wheelv[6];
 
-//double r[] = {650, 650, 100}; //u       //comment for test 
-//double bl[] = {757, 700, 863}; //u
-//double g[] = {844, 634, 858}; //u
-//double br[] = {853, 754, 858}; //u
-//double ye[] = {831, 523, 739}; //u
-//double ora[] = {845, 735, 751}; //u
-//double empty[] = {852, 766, 860}; //u
-//double wheel[] = {};
+double r[] = {31, 31, 23}; //u       //comment for test 
+double bl[] = {21, 25, 35}; //u
+double g[] = {28, 22, 30}; //u
+double br[] = {30, 33, 31}; //u
+double ye[] = {28, 21, 20}; //u
+double ora[] = {30, 34, 20}; //u
+double empty[] = {48, 49, 45}; //u
+double wheel[] = {};
 
-double rv[] = {700,500,100,0,100,0}; //u
-double blv[] = {100, 0, 100,0,700,500}; //u
-double gv[] = {600, 400,100,0,700,500}; //u 
-double brv[] = {853, 754, 858,0,0,0}; //u  
-double yev[] = {831, 523, 739,0,0,0}; //u  
-double orv[] = {845, 735, 751,0,0,0}; //u
-double emptyv[] = {852, 766, 860,0,0,0}; //u
+double rv[] = {60, 10, 80, 0, 40, 10}; //Tren's
+double blv[] = {50, 0, 50, 0, 200, 150}; //u
+double gv[] = {60, 10, 50, 0, 60, 10}; //u 
+double brv[] = {60, 10, 60, 10, 60, 10}; //u  
+double yev[] = {60, 0, 50, 10, 50, 10}; //u  
+double orv[] = {41, 31, 49, 39, 28, 18}; //u
+double emptyv[] = {430, 390, 430, 390, 600, 540}; //u
 double wheelv[] = {};
 
-int lightError = 100;
+int lightError = 5;
 
 int senseSlot = 0;
 int gate1 = 0;
@@ -144,23 +151,23 @@ void setup() {
   qtr.setSensorPins((const uint8_t[]){22,24,26,28,30,32,34,36,38,40,42}, SensorCount);
   qtr.setEmitterPin(2);
   
-//  for(int i = 0; i < 6; i+=2) {       //comment for test 
-//    rv[i] = r[i/2] + lightError; 
-//    rv[i+1] = r[i/2] - lightError;
-//    blv[i] = bl[i/2] + lightError;
-//    blv[i+1] = bl[i/2] - lightError;
-//    gv[i] = g[i/2] + lightError;
-//    gv[i+1] = g[i/2] - lightError;
-//    brv[i] = br[i/2] + lightError;
-//    brv[i+1] = br[i/2] - lightError;
-//    yev[i] = ye[i/2] + lightError;
-//    yev[i+1] = ye[i/2] - lightError;
-//    orv[i] = ora[i/2] + lightError;
-//    orv[i+1] = ora[i/2] - lightError;
-//    emptyv[i] = empty[i/2] + lightError;
-//    emptyv[i+1] = empty[i/2] - lightError;
-//    
-//  }
+  for(int i = 0; i < 6; i+=2) {       //comment for test 
+    rv[i] = r[i/2] + lightError; 
+    rv[i+1] = r[i/2] - lightError;
+    blv[i] = bl[i/2] + lightError;
+    blv[i+1] = bl[i/2] - lightError;
+    gv[i] = g[i/2] + lightError;
+    gv[i+1] = g[i/2] - lightError;
+    brv[i] = br[i/2] + lightError;
+    brv[i+1] = br[i/2] - lightError;
+    yev[i] = ye[i/2] + lightError;
+    yev[i+1] = ye[i/2] - lightError;
+    orv[i] = ora[i/2] + lightError;
+    orv[i+1] = ora[i/2] - lightError;
+    emptyv[i] = empty[i/2] + lightError;
+    emptyv[i+1] = empty[i/2] - lightError;
+    
+  }
   
   for(int i = 0; i < 6; i++) {
     Serial.print("Blue max/mins: "); Serial.println(blv[i]);
@@ -175,7 +182,7 @@ void setup() {
 
 void loop() {
 
-  Serial.print("stopSorting: "); Serial.println(stopSorting);
+  //Serial.print("stopSorting: "); Serial.println(stopSorting);
   
   t_ms = millis();
   t = t_ms / 1000.0;  
@@ -206,6 +213,9 @@ void loop() {
     //Send command to motor
     motorCommand(mp1, mp2, mPWM, input);
 
+    //Send command to hopper motor
+    motorCommand(hop1, hop2, hopPWM, input);
+    
     Pos_old = Pos;
     t_old_enc = t; //save current time and position
 
@@ -223,7 +233,7 @@ void loop() {
 //}
   //Comand
   if(stopSorting) {
-    
+    motorCommand(mp1, mp2, mPWM, 0);
   }
 
 
