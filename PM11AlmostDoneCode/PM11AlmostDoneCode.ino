35ad09e55148f239de68614acf68a7cc93a2e117
Hexalv <trenhir@gmail.com>
2022-04-16 11:33:51 -0600

    bloop

--- PM11AlmostDoneCode/PM11AlmostDoneCode.ino
+++ PM11AlmostDoneCode/PM11AlmostDoneCode.ino
@@ -10,7 +10,7 @@
 //CHANGE FOR EACH PERSON
 int deviceAddress = 3;
 // Color to sort
-int colorDetect = 2; // R=1, G=2. Bl=3, Ye=4, Or=5, Br=6, empty=0
+int colorDetect = 3; // R=1, G=2. Bl=3, Ye=4, Or=5, Br=6, empty=0
 
 // OLED Variables
 #define OLED_RESET 4
